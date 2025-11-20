a1f98fa8c6e8e60889d5ed162b626cbec59c27cc
wills-dev <ejioguvictor6@gmail.com>
2025-11-20 12:28:37 +0100

    added: faq section

--- src/components/molecules/FaqContent/FaqContent.tsx
+++ src/components/molecules/FaqContent/FaqContent.tsx
@@ -3,8 +3,8 @@ import FAQCard from "../FAQCard/FAQCard";
 const FaqContent = () => {
   return (
     <div className="flex-1 flex justify-end">
-      <div className="bg-[#D46CF21F]  w-fit rounded-[12px] py-20">
-        <div className="px-16  border-y border-[#EBD3F2]">
+      <div className="bg-[#D46CF21F]  w-fit rounded-[12px] sm:py-20 py-10">
+        <div className="lg:px-16 md:px-10 px-2  border-y border-[#EBD3F2]">
           <FAQCard />
         </div>
       </div>
