081582bfa1fb097562cdf3ecd2195f536189fce1
victor <ejioguvictor6@gmail.com>
2025-12-14 07:57:40 +0100

    updated: image url

--- src/components/templates/GuideLayout/GuideLayout.tsx
+++ src/components/templates/GuideLayout/GuideLayout.tsx
@@ -9,7 +9,7 @@ interface GuideLayoutProps {
 const GuideLayout = ({ children, title, description }: GuideLayoutProps) => {
   return (
     <div className="max-w-[428px] w-full min-w-[300px] bg-[#FAFAFA] rounded-[16px] border border-gray-200 max-lg:overflow-hidden">
-      <div className="w-full h-[260px] bg-[url('/assets/images/lines.svg')] ">
+      <div className="w-full h-[260px] bg-[url('/assets/images/Lines.svg')] ">
         {children}
       </div>
       <div className="overflow-x-hidden sm:px-4 px-2">
